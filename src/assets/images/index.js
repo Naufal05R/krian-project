ed04017a4c4232735073296f0cf0799eff0da7a2
Naufal05R <naufalrabbaniishere@gmail.com>
2023-06-12 13:45:10 +0700

    update

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,23 +1,23 @@
-import frame11001 from "./frame/frame1x1/frame-1x1-001.png";
-import frame11002 from "./frame/frame1x1/frame-1x1-002.png";
-import frame11003 from "./frame/frame1x1/frame-1x1-003.png";
-import frame11004 from "./frame/frame1x1/frame-1x1-004.png";
-import frame11005 from "./frame/frame1x1/frame-1x1-005.png";
-import frame11006 from "./frame/frame1x1/frame-1x1-006.png";
-import frame11007 from "./frame/frame1x1/frame-1x1-007.png";
-import frame11008 from "./frame/frame1x1/frame-1x1-008.png";
+import frame11001 from "./frame/frame1x1/frame-1x1-001.jpg";
+import frame11002 from "./frame/frame1x1/frame-1x1-002.jpg";
+import frame11003 from "./frame/frame1x1/frame-1x1-003.jpg";
+import frame11004 from "./frame/frame1x1/frame-1x1-004.jpg";
+import frame11005 from "./frame/frame1x1/frame-1x1-005.jpg";
+import frame11006 from "./frame/frame1x1/frame-1x1-006.jpg";
+import frame11007 from "./frame/frame1x1/frame-1x1-007.jpg";
+import frame11008 from "./frame/frame1x1/frame-1x1-008.jpg";
 
-import frame23001 from "./frame/frame2x3/frame-2x3-001.png";
-import frame23002 from "./frame/frame2x3/frame-2x3-002.png";
-import frame23003 from "./frame/frame2x3/frame-2x3-003.png";
-import frame23004 from "./frame/frame2x3/frame-2x3-004.png";
-import frame23005 from "./frame/frame2x3/frame-2x3-005.png";
-import frame23006 from "./frame/frame2x3/frame-2x3-006.png";
-import frame23007 from "./frame/frame2x3/frame-2x3-007.png";
-import frame23008 from "./frame/frame2x3/frame-2x3-008.png";
-import frame23009 from "./frame/frame2x3/frame-2x3-009.png";
-import frame23010 from "./frame/frame2x3/frame-2x3-010.png";
-import frame23011 from "./frame/frame2x3/frame-2x3-011.png";
+import frame23001 from "./frame/frame2x3/frame-2x3-001.jpg";
+import frame23002 from "./frame/frame2x3/frame-2x3-002.jpg";
+import frame23003 from "./frame/frame2x3/frame-2x3-003.jpg";
+import frame23004 from "./frame/frame2x3/frame-2x3-004.jpg";
+import frame23005 from "./frame/frame2x3/frame-2x3-005.jpg";
+import frame23006 from "./frame/frame2x3/frame-2x3-006.jpg";
+import frame23007 from "./frame/frame2x3/frame-2x3-007.jpg";
+import frame23008 from "./frame/frame2x3/frame-2x3-008.jpg";
+import frame23009 from "./frame/frame2x3/frame-2x3-009.jpg";
+import frame23010 from "./frame/frame2x3/frame-2x3-010.jpg";
+import frame23011 from "./frame/frame2x3/frame-2x3-011.jpg";
 
 export {
   frame11001,
